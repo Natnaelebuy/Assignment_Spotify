6b404079342a4c85401d897067cb2dfa5db6ae3a
Natnael <natnaelebuy@gmail.com>
2021-11-17 18:16:28 -0800

    update the code's

--- SpotifyLikeApp.java
+++ SpotifyLikeApp.java
@@ -1,174 +1,95 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Map.Entry;
-
-import javax.lang.model.util.ElementScanner14;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-/*
-   To compile: javac SpotifyLikeApp.java
-   To run: java SpotifyLikeApp
-*/
 
-// declares a class for the app
+
 public class SpotifyLikeApp {
-    
-    // global variables for the app
-    String status;
-    Long position;
-    static Clip audioClip;
-static HashMap<String, Song> song = new HashMap<>();
-private static HashMap<String, Song> songs;
-   // "main" makes this class a java app that can be executed
-   public static void main(String[] args) {
-       Song s = new Song();
-           s.setArtist("CKay");
-           s.setTitle("Love Nwantiti");
-           s.setYear("2020");
-           s.setGenre("Pop");
-           s.setFilePath("./love/CKay - Love Nwantiti.wav");
-           songs.put((String) s.getTitle(), s);
+	
+	JFrame window;
+	Container con;
+	JPanel buttonPanel;
+	JButton soundButton;
+	String clickSound;
+	ButtonHandler bHandler = new ButtonHandler();
+	SoundEffect se = new SoundEffect();
+	
 
-           s = new Song();
-           s.setArtist("Migos");
-           s.setTitle("T-Shirt");
-           s.setYear("2018");
-           s.setGenre("Pop");
-           s.setFilePath("./love/Migos - T-Shirt .wav");
-           songs.put((String) s.getTitle(), s);
+	public static void main(String[] args) {
+		
+		new SpotifyLikeApp();
 
-           s = new Song();
-           s.setArtist("Dj khalid");
-           s.setTitle("hold you down");
-           s.setYear("2015");
-           s.setGenre("Pop");
-           s.setFilePath("./love/Dj khalid- hold you down.wav");
-           songs.put((String) s.getTitle(), s);
-        // create a scanner for user input
-        Scanner input = new Scanner(System.in);
-
-        String userInput = "";
-        while (!userInput.equals("q")) {
-            
-            menu();
-
-            // get input
-            userInput = input.nextLine();
-
-            // accept upper or lower case commands
-            userInput.toLowerCase();
-
-            // do something
-            handleMenu(userInput);
-        }
-
-        /// close the scanner 
-        input.close();
-
-   }
-   
-    /*
-    * displays the menu for the app
-    */
-    private static void menu() {
-
-        System.out.println("---- SpotifyLikeApp ----");
-        System.out.println("[H]ome");
-        System.out.println("[S]earch by title");
-        System.out.println("[L]ibrary");
-        System.out.println("[P]lay");
-        System.out.println("[Q]uit");
-        System.out.println("");
-
-        System.out.println("");
-        System.out.println("Please press the first letter of the menu screen.");
-    }
-
-    /*
-     * handles the user input for the app
-     */
-    private static void handleMenu(String userInput) {
-
-        switch (userInput) {
-            case "h":
-                System.out.println("-->Home<--");            
-                break;
-            case "s":
-                System.out.println("-->Search by title<--");
-                Scanner input = new Scanner(System.in);
-                System.out.println("Please enter the title");
-                String title = input.nextLine();
-                Song song = songs.get(title);
-                
-                if (song !=null) 
-                {
-                    System.out.println("Your current selection is now playing");
-                    play((String) song.getFilePath());
-                    
-                }else {
-                    System.out.println("Sorry, please search again.");
-            }
-                break;
-
-                case "l":
-                System.out.println("-->Library<--");
-                for(Entry<String, Song> s : songs.entrySet()){
-                    System.out.println("Title: " + s.getValue().getTitle());
-                    System.out.println("Artist: " + s.getValue().getArtist());
-                    System.out.println("Gemre: " + s.getValue().getGenre());
-                    System.out.println("Year: " + s.getValue().getYear());
-                    System.out.println("");
-                }
-
-                break;
-
-            case "p":
-                System.out.println("-->Play<--");
-                break;
-
-            case "q":
-                System.out.println("-->Quit<--");
-                break;
-
-            default:
-                break;
-            
-        }
-    }
-    /*
-     * plays an audio file
-     */
-
-    private static void play(String object) {
-
-        // open the audio file
-        final File file = new File(object);
+	}
+	
+	public SpotifyLikeApp(){
+		
+		window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+		con = window.getContentPane();
+		
+		buttonPanel = new JPanel();
+		buttonPanel.setBounds(300, 300, 200, 100);
+		buttonPanel.setBackground(Color.black);
+		con.add(buttonPanel);
+		
+		soundButton = new JButton("Sound Effect");
+		soundButton.setFocusPainted(false);
+		soundButton.addActionListener(bHandler);
+		soundButton.setActionCommand("soundB");
+		buttonPanel.add(soundButton);
+				
+		window.setVisible(true);
+			
+        // Type your audio file name in the res folder
+		clickSound = "./love/Migos - T-Shirt .wav";
 
 
-        try {
-            // create clip
-            audioClip = AudioSystem.getClip();
+	}
+	
+	public class SoundEffect {
+		
+		Clip clip;
+		
+		public void setFile(String soundFileName){
+			
+			try{
+				File file = new File(soundFileName);
+				AudioInputStream sound = AudioSystem.getAudioInputStream(file);	
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+			}
+			catch(Exception e){
+				
+			}
+		}
+		
+		public void play(){
+			
+			clip.setFramePosition(0);
+			clip.start();
+		}
 
-            // get input stream
-            final AudioInputStream in = getAudioInputStream(file);
-
-            audioClip.open(in);
-            audioClip.setMicrosecondPosition(0);
-            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static AudioInputStream getAudioInputStream(File file) {
-        return null;
-    }
-     
-    
-
-
+	}
+	
+	public class ButtonHandler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event){
+			
+			se.setFile(clickSound);
+			se.play();							
+		}
+	}
 
 }
+
---
+++ document.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class document {
+
+    public static List querySelector(String string) {
+        return null;
+    }
+
+    public static List createElement(String string) {
+        return null;
+    }
+
+}
